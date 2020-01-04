ce39fb8bd2a37272b48153b1d0ea1daeff3a328f
Michael Cacciano <michael.cacciano@gmail.com>
2020-01-04 10:46:57 -0500

    modal for adding tech and viewing tech list functioning

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/search-bar/search-bar.component';
 import Logs from './components/logs/logs.component';
 import AddBtn from './components/layout/add-btn/add-btn.component';
+import AddLogModal from './components/add-log-modal/add-log-modal.component';
+import EditLogModal from './components/edit-log-modal/edit-log-modal.component';
+import AddTechModal from './components/add-tech-modal/add-tech-modal.component';
+import TechListModal from './components/tech-list-modal/tech-list-modal.component';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -18,6 +22,10 @@ const App = () => {
       <SearchBar />
       <div className='container'>
         <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
---
+++ src/components/edit-log-modal/edit-log-modal.component.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+import styles from './edit-log-modal.module.css';
+
+const EditLogModal = () => {
+  const [message, setMessage] = useState('');
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState('');
+
+  const onSubmit = e => {
+    if (message === '' || tech === '') {
+      M.toast({ html: 'Please enter a message and tech', classes: 'red' });
+    } else {
+      console.log(message, tech, attention);
+
+      // clear fields
+      setMessage('');
+      setTech('');
+      setAttention(false);
+    }
+  };
+
+  return (
+    <div id='edit-log-modal' className='modal' style={styles.EditLogModal}>
+      <div className='modal-content'>
+        <h4>Edit System Log</h4>
+        <div className='row'>
+          <div className='input-field'>
+            <input type='text' name='message' value={message} onChange={e => setMessage(e.target.value)} />
+            <label htmlFor='message' className='active'>
+              Log Message
+            </label>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='input-field'>
+            <select className='browser-default' name='tech' value={tech} onChange={e => setTech(e.target.value)}>
+              <option value='' disabled>
+                Select Technician
+              </option>
+              <option value='John Doe'>John Doe</option>
+              <option value='Sam Smith'>Sam Smith</option>
+              <option value='Sara Wilson'>Sara Wilson</option>
+              <option value='Mozzey Magick'>Mozzey Magick</option>
+            </select>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='input-field'>
+            <p>
+              <label>
+                <input
+                  type='checkbox'
+                  className='filled-in'
+                  checked={attention}
+                  value={attention}
+                  onChange={e => setAttention(!attention)}
+                />
+                <span>Needs Attention</span>
+              </label>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='modal-footer'>
+        <a href='#!' onClick={onSubmit} className='modal-close waves-effect blue waves-light btn'>
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default EditLogModal;
